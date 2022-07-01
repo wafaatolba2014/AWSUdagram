bb57d3f1b2268c510000f086148c19266e9059de
wafaatolba2014 <wafaatolba@hotmail.com>
2022-07-01 23:24:06 +0300

    update back-end eb init deploy.sh

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,3 +1,6 @@
-eb init
-eb deploy Quotes-env
-eb setenv PORT=$PORT ENV=$ENV DB_USER=$DB_USER DB_PASSWORD=$DB_PASSWORD DB_HOST=$DB_HOST DB_NAME=$DB_NAME DB_PORT=$DB_PORT
+eb init udagram-cli-api --platform node.js --region us-east-1
+eb use udagram-cli-api-dev
+
+eb deploy
+
+eb setenv POSTGRES_HOST=database-1.cxbkefpagwjw.us-east-1.rds.amazonaws.com POSTGRES_DB=postgres POSTGRES_USERNAME=postgres POSTGRES_PASSWORD=postgres123 PORT=8080 DB_PORT=5432 RDS_DIALECT=postgres AWS_REGION=us-east-1 AWS_ACCESS_KEY_ID=AKIASLVP5AGW6KKIJ45N AWS_SECRET_ACCESS_KEY=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 AWS_S3_ENDPOINT=http://udagram-bucket-1.s3-website-us-east-1.amazonaws.com AWS_PROFILE=default AWS_BUCKET=udagram-bucket-1 URL=http://localhost JWT_SECRET=123
