9e280339dba4637d67b486e7b9825bcada6670b1
wafaatolba2014 <wafaatolba@hotmail.com>
2022-07-02 23:35:26 +0300

    build edit

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,2 +1,3 @@
-eb deploy Quotes-env
+eb use udagram-cli-api2-dev
+eb deploy udagram-cli-api2-dev
 eb setenv PORT=$PORT ENV=$ENV DB_USER=$DB_USER DB_PASSWORD=$DB_PASSWORD DB_HOST=$DB_HOST DB_NAME=$DB_NAME DB_PORT=$DB_PORT
